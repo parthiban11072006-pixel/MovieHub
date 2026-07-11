ee536ca5b59f9066aa8248ae27b32c6deace508d
Your Name <your-email@example.com>
2026-07-11 13:12:46 +0530

    Fix config and routes

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import tailwindcss from '@tailwindcss/vite' // அல்லது உங்கள் வெர்ஷனுக்கு ஏற்ப
 
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
+  plugins: [react(), tailwindcss()],
+  base: "/MovieHub/", 
 })
