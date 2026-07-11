5669a9a070f6a35b8bdd6ab17f96d943cf1bea0c
Your Name <your-email@example.com>
2026-07-11 13:35:36 +0530

    update base path to dot slash

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite' // அல்லது உங்கள் வெர்ஷனுக்கு ஏற்ப
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/MovieHub/", 
+  base: "./", 
 })
