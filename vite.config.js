a2b080963f1fd9e39e22b6d5bd5099cc245fb0a8
Your Name <your-email@example.com>
2026-07-12 05:03:16 +0530

    update base path

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: "/MovieHub/", // உங்கள் ரிபோசிட்டரி பெயர் 'MovieHub' என்றால் இது சரியாக இருக்க வேண்டும்
+  base: "/movieHub/",
 })
